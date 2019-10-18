61fe3e354fca62fcd60aad89b2c08005510d2cb7
avangelizer <56427459+avangelizer@users.noreply.github.com>
2019-10-18 17:24:31 +0200

    Update config.py
    added index of images to be dropped from the series

--- underwater/underwater/config.py
+++ underwater/underwater/config.py
@@ -1,1 +1,156 @@
-""""""
+
+index=list(range(0,141))
+index.extend([142,147,148,153,160,165,168,169,173,181,183,186,187,188,189,190,192,193,201,202,203,210,211,215,216,218,222,226,228,232,233,236,237,242,245,262])
+index.extend(list(range(264,268)))
+index.extend([269,279,280,282,284,286,287,291,292,293,296,297,299,300,301,305,309, 382,396,406,413,426,428,433,438,440,442,451,
+              453,457,461,469,470,476,490,491,496,505,520,528,530,534,537,538,550,554,555,559,562,566,577, 
+              591,593,605,607,626,630,636,638,646,650,652,662,674,679,684,689,692,693,729,730,802,810,816,817,820,822,824, 830,
+             846, 8551,854,864,885,886,892,898, 909,911,938,948,956,985,990,996,1000,1005,1006,1024,1032,1036,1037,1039,1042,
+              1043,1045,1050,1059,1061,1065,1074,1080,1084,1105,1109,1122,1126,1128,1141,1147,1154,1155,
+             1190,1193,1197,1259,1260,1283,1289,1293,1346,1347,1373,1390,1400,1405,1409,1416,1417,1427,1465,1471,1476,1494,
+              1506,1507,1509,1521,1528,1706,1709,1713,1730,1740,1741,1748,1751,1768,1770,1771,1796,1799,1801,1802,1812,1823,1828,1831,1843,1846,
+              1873,1874,1942,1946,1954,1963,1969,1971,1980,1999,2004,2009,2010,2016,2024,2035,2036,2036,2042,2043,
+              2058,2060,2072,2081,2086,2089,2091,2100,2101,2104,2107,2116,2125,2126,2142,2144,2147,2156,2177,2181,
+              2248,2371,2376,2385,2386,2398,2406,2426,2443,2444,2476,2505,2507,2530,2625,2627,2628,2633,2634,2638,
+              2750,2755,2762,2777,2784,2790,2806,2803,2806,2808,2813,2818,2829,2836,2838,2840,2841,2848,2854,2863,
+              2874,2880,2887,2889,2890,2894,2897,2898,2901,2910,2913,2914,2915,2924,2929,2936,2940,2951,2959,2961,
+              2967,2970,2978,2980,2987,2992,3001,3005,3014,3020,3028,3032,3036,3037,3039,3040,3066,3067,3071,3072,3075,
+              3083,3084,3086,3089,3090,3091,3093,3097,3100,3102,3108,3113,3114,3117,3121,3126,3128,3151,3173,3174,3176,
+              3178,3179,3181,3182,3185,3190,3191,3197,3201,3202,3236,3240,3244,3246,3248,3249,3252,3253,3256,
+              3262,3263,3335,3336,3353,3356,3361,3366,3367,3448,3461,3462,3466,3479,3480,3482,3490,3491,3502,3508,3513,3519,3523,3524,3525,
+              3540,3541,3551,3570,3571,3580,3584,3592,3593,3597,3601,3603,3610,3613,3614,3615,3620,3622,3623,3635,3636,3637,3644,3674,3691,
+              3692,3701,3705,3706,3723,3728,3729,3733,3734,3739,3749,3752,3761,3844,3847,3851,3856,3870,3873,3875,3878,3888,3892,3899,3905,
+              3910,3923,3928,3929,3938,3942,3946,3949,3965,3968,3969,3970,3971,3994,3995,4003,4004,4025,4027,4031,4036,4039,
+              4046,4051,4058,4065,4069,4071,4072,4093,4094,4096,4097,4100,4104,4106,4107,4105,4112,4113,4206,4212,4218,4222,
+              4228,4229,4230,4231,4233,4239,4246,4260,4324,4332,4333,4335,4337,4340,4342,4344,4356,4357,4382,4401,4405,4409,
+              4429,4430,4432,4434,4446,4450,4454,4532,4533,4534,4542,4560,4564,4572,4575,4577,4696,4730,4732,4734,4741,4745,
+              4762,4764,4791,4922,4934,4941,4959,4963,4967,4968,4971,4981,4985,4986
+
+             ])
+index.extend(list(range(315,369)))
+
+index.extend(list(range(385,391)))
+index.extend(list(range(541,544)))
+index.extend(list(range(545,549)))
+index.extend(list(range(572,575)))
+                   
+index.extend(list(range(582,585)))
+                  
+index.extend(list(range(702,722)))
+index.extend(list(range(735,780)))
+                   
+index.extend(list(range(832,835)))
+                   
+index.extend(list(range(919,922)))
+                   
+index.extend(list(range(1090,1105)))
+                   
+index.extend(list(range(1114,1117)))
+
+index.extend(list(range(1165,1180)))
+index.extend(list(range(1182,1184)))
+
+index.extend(list(range(1200,1257)))
+                   
+index.extend(list(range(1296,1305)))
+index.extend(list(range(1307,1342)))
+                   
+index.extend(list(range(1431,1445)))
+index.extend(list(range(1449,1455)))
+index.extend(list(range(1499,1501)))
+                   
+index.extend(list(range(1511,1516)))
+                   
+index.extend(list(range(1529,1700)))
+                   
+index.extend(list(range(1755,1759)))
+                   
+index.extend(list(range(1773,1781)))
+index.extend(list(range(1782,1785)))
+index.extend(list(range(1787,1794))) 
+index.extend(list(range(1850,1870)))
+index.extend(list(range(1875,1940)))
+
+index.extend(list(range(2045,2057)))
+index.extend(list(range(2134,2138)))
+index.extend(list(range(2160,2172)))
+index.extend(list(range(2183,2237)))
+
+index.extend(list(range(2260,2263)))
+index.extend(list(range(2347,2350)))
+index.extend(list(range(2351,2354)))
+index.extend(list(range(2355,2361)))
+index.extend(list(range(2364,2368)))
+                        
+index.extend(list(range(2410,2415)))
+index.extend(list(range(2416,2420)))
+
+index.extend(list(range(2490,2494)))
+index.extend(list(range(2509,2525)))
+index.extend(list(range(2544,2602)))
+index.extend(list(range(2621,2625)))
+index.extend(list(range(2651,2742)))
+index.extend(list(range(2764,2769)))
+index.extend(list(range(2856,2862)))
+index.extend(list(range(2865,2869)))
+index.extend(list(range(2871,2873)))
+index.extend(list(range(2918,2923)))
+                        
+index.extend(list(range(2925,2927)))
+                        
+index.extend(list(range(3047,3053)))
+index.extend(list(range(3055,3060)))
+index.extend(list(range(3062,3065)))
+                        
+index.extend(list(range(3077,3080)))
+                        
+index.extend(list(range(3140,3142)))
+index.extend(list(range(3140,3147)))
+index.extend(list(range(3148,3151)))
+                        
+index.extend(list(range(3154,3159)))
+index.extend(list(range(3159,3169)))
+
+index.extend(list(range(3257,3261)))
+index.extend(list(range(3278,3335)))
+index.extend(list(range(3377,3439)))
+index.extend(list(range(3530,3537)))
+index.extend(list(range(3545,3550)))
+index.extend(list(range(3557,3561)))
+index.extend(list(range(3617,3620)))
+index.extend(list(range(3626,3629)))
+index.extend(list(range(3640,3643)))
+index.extend(list(range(3648,3656)))
+index.extend(list(range(3657,3664)))
+index.extend(list(range(3665,3668)))
+index.extend(list(range(3676,3680)))
+index.extend(list(range(3740,3743)))
+index.extend(list(range(3745,3748)))
+index.extend(list(range(3765,3830)))
+index.extend(list(range(3930,3935)))
+index.extend(list(range(3961,3964)))
+index.extend(list(range(4005,4010)))
+index.extend(list(range(4042,4045)))
+index.extend(list(range(4081,4085)))
+index.extend(list(range(4085,4092)))
+index.extend(list(range(4126,4181)))
+index.extend(list(range(4193,4200)))
+index.extend(list(range(4201,4204)))
+index.extend(list(range(4264,4301)))
+index.extend(list(range(4304,4307)))
+index.extend(list(range(4313,4316)))
+index.extend(list(range(4327,4330)))
+index.extend(list(range(4350,4355)))
+index.extend(list(range(4359,4381)))
+index.extend(list(range(4436,4440)))
+index.extend(list(range(4441,4445)))
+index.extend(list(range(4459,4515)))
+index.extend(list(range(4603,4638)))
+index.extend(list(range(4639,4650)))
+index.extend(list(range(4651,4691)))
+index.extend(list(range(4701,4710)))
+index.extend(list(range(4751,4755)))
+index.extend(list(range(4774,4785)))
+index.extend(list(range(4800,4895)))
+index.extend(list(range(4912,4920)))
+index.extend(list(range(4993,4998)))
